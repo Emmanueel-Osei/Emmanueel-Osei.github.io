099e7674ed38f5b8b20c451111d160ad13170794
Emmanueel-Osei <emma543216789@gmail.com>
2026-01-08 18:52:46 +0000

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 (function(){
   const questions = [
     {q:' In HTML5, which element is used to embed scalable vector graphics directly into a webpage?', choices:['<img>','<canvas>','<svg>','<object>'], a:2},
-    {q:'In CSS, what is the difference between relative and absolute positioning?', choices:['Relative is positioned relative to the parent element, absolute is positioned relative to the viewport','Relative is positioned relative to the viewport, absolute is positioned relative to the parent element','Both are positioned relative to the parent element','Both are positioned relative to the viewport'], a:0},
+    {q:'In CSS, what is the difference between relative and absolute positioning?', choices:['Relative is positioned relative to the parent element, Absolute is positioned relative to the viewport','Relative is positioned relative to the viewport, Absolute is positioned relative to the parent element','Both are positioned relative to the parent element','Both are positioned relative to the viewport'], a:0},
     {q:'What is the main difference between localStorage and sessionStorage in web browsers?', choices:['localStorage stores data permanently, sessionStorage stores until the tab/session ends','Both store data permanently','Both store data until the cache is cleared','localStorage stores data until browser is closed,sessionStorage stores permanently'], a:0},
     {q:"In responsive design, what does the CSS unit 'vw' represent?", choices:['Variable width','Viewport width','Vertical width','Virtual window'], a:1},
     {q:'Which JavaScript method is used to stop event bubbling in the DOM?', choices:['event.stopImmediatePropagation()','event.cancelBubble()','event.stopPropagation()','event.preventDefault()'], a:2}
@@ -43,3 +43,4 @@
     render();
   });
 })();
+
